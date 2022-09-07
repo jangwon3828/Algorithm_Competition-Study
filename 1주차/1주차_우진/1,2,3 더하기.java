ad70240e9e960661603a94f0d686c499b27b0ae6
WooJinDeve <jwj060111@gmail.com>
2022-09-07 19:36:31 +0900

    1,2,3 더하기_9095

---
+++ 1주차/1주차_우진/1,2,3 더하기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class 1,2,3 더하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[11];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 4;
+
+        for(int i = 4; i<11; i++){
+            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+        }
+
+        while (N-- > 0) {
+            System.out.println(arr[sc.nextInt()]);
+        }
+    }
+}
