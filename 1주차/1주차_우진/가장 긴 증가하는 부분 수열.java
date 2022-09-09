cd136eb82262f9452adf78c7c884bbd9e5bfd905
WooJinDeve <jwj060111@gmail.com>
2022-09-09 12:41:26 +0900

    가장 긴 증가하는 부분 수열_11051

---
+++ 1주차/1주차_우진/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class 가장 긴 증가하는 부분 수열 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i=0; i<n; i++) a[i] = sc.nextInt();
+]
+        int[] d = new int[n];
+        for (int i=0; i<n; i++) {
+            d[i] = 1;
+            for (int j=0; j<i; j++) {
+                if (a[j] < a[i]) {
+                    d[i] = Math.max(d[i], d[j]+1);
+                }
+            }
+        }
+        int ans = d[0];
+        for (int i=0; i<n; i++) {
+            if (ans < d[i]) {
+                ans = d[i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
