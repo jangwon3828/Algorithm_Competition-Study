c89be6c58e63cd12453a16f6675f20c6b90f8cba
WooJinDeve <jwj060111@gmail.com>
2022-09-11 21:33:01 +0900

    정수삼각형_1932

---
+++ 1주차/1주차_우진/정수 삼각형.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class 정수 삼각형 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] a = new int[n][n];
+        int[][] d = new int[n][n];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<=i; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        d[0][0] = a[0][0];
+        for (int i=1; i<n; i++) {
+            for (int j=0; j<=i; j++) {
+                d[i][j] = d[i-1][j] + a[i][j];
+                if (j-1 >= 0 && d[i][j] < d[i-1][j-1] + a[i][j]) {
+                    d[i][j] = d[i-1][j-1] + a[i][j];
+                }
+            }
+        }
+        int ans = d[n-1][0];
+        for (int i=0; i<n; i++) {
+            if (ans < d[n-1][i]) {
+                ans = d[n-1][i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
