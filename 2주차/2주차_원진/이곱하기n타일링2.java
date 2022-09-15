e77a5f116b88d259be3059a20dd11758fff8b54a
wonjin <jang3828@gamil.com>
2022-09-15 20:18:38 +0900

    2xN 타일링 2_11727

--- 2주차/2주차_원진/이곱하기n타일링2.java
+++ 2주차/2주차_원진/이곱하기n타일링2.java
@@ -9,7 +9,7 @@ public class 이곱하기n타일링 {
             d[0] = 1;
             d[1] = 1;
             for (int i=2; i<=n; i++) {
-                d[i] = d[i-1] + d[i-2];
+                d[i] = d[i-1] + d[i-2]*2;
                 d[i] %= 10007;
             }
             System.out.println(d[n]);
