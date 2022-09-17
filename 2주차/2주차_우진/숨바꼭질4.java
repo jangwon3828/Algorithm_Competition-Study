08fcc203242573371e6604160ec813f0bcaf3931
WooJinDeve <jwj060111@gmail.com>
2022-09-17 22:48:00 +0900

    숨바꼭질 4_13913

---
+++ 2주차/2주차_우진/숨바꼭질4.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class 숨바꼭질 4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] visit = new int[100001];
+        int[] check = new int[100001];
+
+        Queue<Integer> q = new LinkedList<>();
+        visit[N] = 1;
+        q.add(N);
+
+        int[] s = new int[3];
+
+        while (!q.isEmpty()) {
+            int next = q.poll();
+
+            if (next == K) break;
+
+            s[0] = next + 1;
+            s[1] = next - 1;
+            s[2] = next * 2;
+
+            for (int i = 0; i < 3; i++) {
+                if (s[i] <= 100000 && s[i] >= 0 && visit[s[i]] == 0) {
+                    visit[s[i]] = visit[next] + 1;
+                    check[s[i]] = next;
+                    q.add(s[i]);
+                }
+            }
+        }
+        System.out.println(visit[K] - 1);
+        print(check, N, K);
+    }
+
+    static void print(int[] from, int N, int K) {
+        if (N != K) {
+            print(from, N, from[K]);
+        }
+        System.out.print(K + " ");
+    }
+}
