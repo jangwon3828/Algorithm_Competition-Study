888cddbf012c656d0dd33472527fc68dcaca037e
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2022-09-19 23:22:21 +0900

    영화감독 숌_1436

--- 2주차/2주차_우현/영화감독 숌.java
+++ 2주차/2주차_우현/영화감독 숌.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 import java.io.*;
 
@@ -20,27 +19,4 @@ public class Main {
 
         System.out.println(num);
     }
-=======
-import java.util.*;
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        int num = 666;
-        int count = 1;
-
-        while (count != N) {
-            num++;
-            if(String.valueOf(num).contains("666")) {
-                count++;
-            }
-        }
-
-        System.out.println(num);
-    }
->>>>>>> 3d310144038cc2177dad2724a9ed737a8fc365ea
 }
