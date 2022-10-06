f517138340e2536c94af86a516fba6a8d32ccd00
jangwon3828 <wkd3828@gmail.com>
2022-10-06 17:11:56 +0900

    연산자끼워넣기2_15658

---
+++ 5주차/5주차_원진/연산자끼워넣기2.java
@@ -0,0 +1,57 @@
+package baekjoon;
+
+import java.util.*;
+
+public class 연산자끼워넣기2 {
+    static Pairs calc(int[] a, int index, int cur, int plus, int minus, int mul, int div) {
+        int n = a.length;
+        if (index == n) {
+            return new Pairs(cur, cur);
+        }
+        ArrayList<Pairs> res = new ArrayList<>();
+        if (plus > 0){
+            res.add(calc(a,index+1,cur+a[index],plus-1,minus,mul,div));
+        }
+        if (minus > 0) {
+            res.add(calc(a,index+1,cur-a[index],plus,minus-1,mul,div));
+        }
+        if (mul > 0) {
+            res.add(calc(a,index+1,cur*a[index],plus,minus,mul-1,div));
+        }
+        if (div > 0) {
+            res.add(calc(a,index+1,cur/a[index],plus,minus,mul,div-1));
+        }
+        Pairs ans = res.get(0);
+        for (Pairs p : res) {
+            if (ans.max < p.max) {
+                ans.max = p.max;
+            }
+            if (ans.min > p.min) {
+                ans.min = p.min;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i=0; i<n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int plus = sc.nextInt();
+        int minus = sc.nextInt();
+        int mul = sc.nextInt();
+        int div = sc.nextInt();
+        Pairs ans = calc(a, 1, a[0], plus, minus, mul, div);
+        System.out.println(ans.max);
+        System.out.println(ans.min);
+    }
+}
+class Pair {
+    public int min, max;
+    Pair(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+}
